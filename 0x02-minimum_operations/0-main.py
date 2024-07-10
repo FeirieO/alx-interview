716a40795777c1f9ce4fc5c9d4b93946dfdad7eb
FeirieO <feirie001@gmail.com>
2024-07-10 10:38:12 +0100

    fixed algorithms omittion

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,12 +5,8 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-
-n = 2 * 2 * 2
+n = 4
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-n = 44
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,14 @@
-#!/usr/bin/python3
-"""
-Script that computes a minimum operations
-needed in a CopyAll - Paste task
-"""
-
-
 def minOperations(n):
-    """
-    Method for computing the minimum number
-    of operations for task Copy All and Paste
-    Args:
-        n: input value
-    Return: the sum of the operations
-    """
-    if n < 2:
+    if n <= 1:
         return 0
-    factor_sum = 0
-    i = 2  # Start with the first prime number
+    
+    operations = 0
+    factor = 2
+    
     while n > 1:
-        if n % i == 0:
-            factor_sum += i
-            n //= i  # Use integer division
-        else:
-            i += 1
-    return factor_sum
-
-# Example usage
-result = minOperations(12)
-print("Minimum operations:", result)
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
